8c6dd4780e6a1013971b821697897e651e317821
tempura327 <cks6658324@yahoo.com.tw>
2021-11-07 21:50:14 +0800

    import vuex, router, bootstrap-vue.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import {BootstrapVue} from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false
 
