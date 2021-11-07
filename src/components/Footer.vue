47769df4eda42a3f7e8437365e7455c23e659f3a
tempura327 <cks6658324@yahoo.com.tw>
2021-11-07 21:49:19 +0800

    [component] added Header and Footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="footer">
+        CopyRight :  Travel Taiwan    Design By :  Tracy
+    </footer>
+</template>
+
+<style scoped>
+    .footer{
+        background-color: #08A6BB;
+        font-weight: 700;
+        text-align: center;
+        color: white;
+        height: 120px;
+        line-height: 120px;
+    }
+</style>
