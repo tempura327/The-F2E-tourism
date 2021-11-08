d6d15dd0fa4b91875577e0bd225c75b13f0b5e65
tempura327 <cks6658324@yahoo.com.tw>
2021-11-08 19:46:01 +0800

    [component] added Card.

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-card>
+        <img src="" alt="" class="mb-6 ">
+        <h2 class="des h2"></h2>
+    </b-card>
+</template>
+
+<style scoped>
+    .des{
+        color: #313131;
+        font-weight: 700;
+    }
+</style>
