81b35a62d88b460381483625e32c6ee9520e6634
tempura327 <cks6658324@yahoo.com.tw>
2021-11-09 20:35:28 +0800

    import bootstrap style.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import Vuex from 'vuex'
-import {BootstrapVue} from 'bootstrap-vue'
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Vuex from 'vuex';
+import {BootstrapVue,LayoutPlugin} from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
-Vue.use(VueRouter);
+Vue.use(LayoutPlugin);
+Vue.use(router);
 Vue.use(Vuex);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
