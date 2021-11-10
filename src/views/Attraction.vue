078a01bec6f096cdaa21230bb294cecd3fc20b48
tempura327 <cks6658324@yahoo.com.tw>
2021-11-10 22:18:34 +0800

    [page[ added Attraction. and modified Home.

--- src/views/Attraction.vue
+++ src/views/Attraction.vue
@@ -1,8 +1,8 @@
 <template>
-<b-container>
+    <b-container>
       <b-row>
         <b-col>
-          <h1 class="big-title mb-6">熱門景點</h1>
+          <h1 class="big-title mb-6">景點</h1>
           <h5 class="h5 description mb-12">
             台灣的各個美景，都美不勝收。<br>
             等你一同來發現這座寶島的奧妙！
@@ -39,49 +39,17 @@
           <Card></Card>
         </b-col>
       </b-row>
-
-      <b-row>
-        <b-col>
-          <h1 class="big-title mb-6">活動類別</h1>
-          <h5 class="h5 description mb-12">
-            各種不同的活動內容<br>
-            邀請您一銅來共襄盛舉！
-          </h5>
-        </b-col>
-      </b-row>
-
-      <b-row  class="mb-25">
-        <b-col>
-          <SimpleCard title="XXX" imgSrc="https://images.unsplash.com/photo-1542312743-e4a4a04f412a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80"></SimpleCard>
-        </b-col>
-        <b-col>
-          <SimpleCard></SimpleCard>
-        </b-col>   
-        <b-col>
-          <SimpleCard></SimpleCard>
-        </b-col>
-        <b-col>
-          <SimpleCard></SimpleCard>
-        </b-col>               
-      </b-row>
-
-      <!-- <b-row>
-          <b-col>
-              <img src="../assets/unsplash_dQejX2ucPBs.png" alt="">
-          </b-col>
-      </b-row> -->
     </b-container>
 </template>
 
 <script>
   import Card from '../components/Card.vue';
-  import SimpleCard from '../components/SimpleCard.vue';
 
   export default({
-      name:'Home',
+      name:'Attraction',
       components:{
           Card,
-          SimpleCard,
+
       }
   })
 </script>
