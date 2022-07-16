2c1af6dae9d25f5af700fea3676dfd4e53c99041
tempura327 <cks6658324@yahoo.com.tw>
2022-07-16 14:47:03 +0800

    [refactor] modified structure and style of Card and SimpleCard.

--- src/components/SimpleCard.vue
+++ src/components/SimpleCard.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <div class="mb-6 card" :style="`background-image: url(${imgSrc}); background-size: cover;`" @click="showModal(data, 'activity')"></div>
-    <h2 class="card-title h2 text-center" @click="showModal(data, 'activity')">
-      {{ title }}
-    </h2>
+  <div @click="showModal(data, 'activity')">
+    <slot>
+      <div class="card_img rounded-lg h-48 mb-6" :style="`background-image: url(${data.Picture.PictureUrl1}); background-size: cover;`"></div>
+
+      <h2 class="card_title h2 text-center" @click="showModal(data, 'activity')">
+        {{ data.ActivityName }}
+      </h2>
+
+      <h2 class="h2 text-center">{{ data.StartTime.slice(0, 10) }}</h2>
+    </slot>
   </div>
 </template>
 
@@ -25,26 +30,3 @@
     }
   }
 </script>
-
-<style scoped lang="scss">
-  // .card-title,
-  // .card {
-  //   cursor: pointer;
-  // }
-
-  // .card-title:hover {
-  //   color: #08a6bb;
-  // }
-
-  // .card {
-  //   width: 100%;
-  //   height: 324px;
-  //   border-radius: 10px;
-  //   box-shadow: 2px 2px 4px rgba(114, 142, 171, 0.1), -6px -6px 20px rgba(255, 255, 255, 0.6), 4px 4px 20px rgba(111, 140, 176, 0.41);
-  // }
-
-  // .card-img {
-  //   width: 100%;
-  //   height: 100%;
-  // }
-</style>
