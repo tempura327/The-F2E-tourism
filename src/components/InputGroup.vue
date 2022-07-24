9d6b10ebb8920abc9c2cff3af13d29595c007e23
tempura327 <cks6658324@yahoo.com.tw>
2022-07-24 14:29:44 +0800

    [feat] added InputGroup

---
+++ src/components/InputGroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="group">
+    <slot name="input-group-prepend"></slot>
+
+    <slot></slot>
+
+    <slot name="input-group-append"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class InputGroup extends Vue {
+    // data
+    // hook
+    // methods
+  }
+</script>
