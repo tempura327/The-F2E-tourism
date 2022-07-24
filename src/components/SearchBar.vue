f84438e6c734607f5cdfdefad1898766a908819f
tempura327 <cks6658324@yahoo.com.tw>
2022-07-24 14:31:21 +0800

    [feat] added SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <InputGroup>
+    <template #input-group-prepend>
+      <select name="type" id="type" v-model="search.type">
+        <option value="">全部</option>
+        <option value="attraction">景點</option>
+        <option value="activity">活動</option>
+      </select>
+    </template>
+
+    <input v-model="search.keyword" class="group_input" type="text" />
+
+    <template #input-group-append>
+      <button class="btn" @click="searchClick">
+        <img src="../assets/search.svg" alt="" />
+      </button>
+    </template>
+  </InputGroup>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  import InputGroup from '@/components/InputGroup.vue';
+
+  @Component({
+    components: {
+      InputGroup,
+    },
+  })
+  export default class SearchBar extends Vue {
+    // data
+    search: { keyword: string; type: string } = {
+      keyword: '',
+      type: '',
+    };
+    // hook
+    // methods
+    searchClick(): void {
+      this.$emit('searchClick', this.search);
+    }
+  }
+</script>
