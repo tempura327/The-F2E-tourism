2c3661cfcfb8f31b7b2f907b2f29d136f7b454ae
tempura327 <cks6658324@yahoo.com.tw>
2022-08-29 22:08:08 +0800

    [refactor] modified onMarkerClick() and finished ToolTip in Calendar.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -25,7 +25,7 @@
       </div>
     </div>
 
-    <div v-for="(day, index) in weekDayArray" :key="`day-${index}`" class="calendar_cell bg-primary text-white font-bold">
+    <div v-for="(day, index) in weekDayArray" :key="`day-${index}`" class="calendar_cell bg-primary font-bold text-white">
       {{ day }}
     </div>
 
@@ -34,12 +34,69 @@
     </div>
 
     <template v-else>
-      <div v-for="(date, index) in currentDateArray" :key="`date-${index}`" class="calendar_cell date text-gray-80">
+      <div v-for="(date, index) in currentDateArray" :key="`date-${index}`" class="calendar_cell date">
         {{ date }}
-        <div class="calendar_marker" v-for="(item, index) in activityData[date]?.slice(0, 3)" :key="`${date}-${index}`">{{ item.ActivityName }}</div>
-        <div v-if="activityData[date]?.length > 3">...</div>
+        <div
+          class="calendar_marker"
+          v-for="(item, index) in activityData[date]?.slice(0, 3)"
+          :key="`${date}-${index}`"
+          @click="onMarkerClick($event.target, item)">
+          {{ item.ActivityName }}
+        </div>
+
+        <div v-if="activityData[date]?.length > 3" class="cursor-pointer" @click="showMoreActivity($event.target, activityData[date])">...</div>
       </div>
     </template>
+
+    <Tooltip :isShow="isTooltipSHow" :position="position">
+      <template #control>
+        <div class="flex mb-2">
+          <button class="">
+            <font-awesome-icon v-show="isActivitySelected" class="text-gray-80 ml-auto mb-2 hover:scale-150 cursor-pointer" icon="angle-left" />
+          </button>
+
+          <button class="ml-auto" @click="isTooltipSHow = !isTooltipSHow">
+            <font-awesome-icon class="text-gray-80 ml-auto mb-2 hover:scale-150 cursor-pointer" icon="xmark" />
+          </button>
+        </div>
+      </template>
+
+      <div class="grid gap-2 grid-cols-2" v-if="!isActivitySelected">
+        <div
+          class="calendar_marker"
+          v-for="(item, index) in selectedDateActivity"
+          :key="`selected-${index}`"
+          @click="onMarkerClick($event.target, item)">
+          {{ item.ActivityName }}
+        </div>
+      </div>
+
+      <section class="flex flex-col" v-else>
+        <!-- if activity is added to google calendar show this. -->
+        <!-- <button> -->
+        <!-- <font-awesome-icon class="text-primary ml-auto mb-2 hover:scale-150 cursor-pointer" :icon="['fas', 'bookmark']" /> -->
+        <!-- </button> -->
+
+        <div class="flex items-center">
+          <button class="mr-2">
+            <font-awesome-icon class="text-primary text-h4 ml-auto mb-2 hover:scale-125 cursor-pointer" :icon="['far', 'bookmark']" />
+          </button>
+
+          <h2 class="text-h2 text-gray-80 font-bold mb-4">
+            {{ info.ActivityName }}
+            <span class="text-h5">/ {{ info.Class1 || '未分類' }}</span>
+          </h2>
+        </div>
+
+        <Gallery :images="images" galleryClass="mb-4"></Gallery>
+
+        <h4 class="text-h5 text-gray-80 mb-2">{{ extractDateRangeStr(`${info.StartTime}~${info.EndTime}`) }}</h4>
+        <h4 class="text-h5 text-gray-80 mb-2">{{ info.Address }}</h4>
+        <h4 class="text-h5 text-gray-80 mb-2">{{ info.Phone }}</h4>
+        <hr class="my-3" v-if="info.Description" />
+        <h4 class="text-h5 text-gray-80">{{ info.Description }}</h4>
+      </section>
+    </Tooltip>
   </div>
 </template>
 
@@ -47,13 +104,17 @@
   import { Component, Vue, Prop } from 'vue-property-decorator';
 
   import Spinner from '@/components/Spinner.vue';
+  import Tooltip from '@/components/Tooltip.vue';
+  import Gallery from '@/components/Gallery.vue';
 
-  import dateConvertor from '@/utility/dateConvertor';
+  import dateConvertor, { extractDateRangeStr } from '@/utility/dateConvertor';
   import { activity, current } from '@/utility/type';
 
   @Component({
     components: {
       Spinner,
+      Tooltip,
+      Gallery,
     },
   })
   export default class Calendar extends Vue {
@@ -67,6 +128,9 @@
     activityData!: { [key: string]: activity[] };
 
     // data
+    extractDateRangeStr = extractDateRangeStr;
+    isTooltipSHow = false;
+    isActivitySelected = false;
     weekDayArray = ['日', '一', '二', '三', '四', '五', '六'];
     todayDateObj = new Date();
     today = {
@@ -80,6 +144,27 @@
       startDateObj: undefined,
       year: 0,
     };
+    info: activity = {
+      ActivityID: '',
+      ActivityName: '',
+      City: '',
+      Description: '',
+      EndTime: '',
+      Location: '',
+      Organizer: '',
+      Picture: {},
+      Position: {
+        GeoHash: '',
+        PositionLat: 0,
+        PositionLon: 0,
+      },
+      SrcUpdateTime: '',
+      StartTime: '',
+      UpdateTime: '',
+    };
+    position = [0, 0];
+    selectedDateActivity: activity[] = [];
+    images: string[] = [];
 
     // hook
     created(): void {
@@ -119,6 +204,45 @@
       this.current.endDateObj = new Date(this.current.year, this.current.month + 1, 0);
 
       this.$emit('onCurrentChange', this.current);
+    }
+    onMarkerClick(ele: HTMLElement, data: activity): void {
+      this.info = data;
+      this.images = Object.values(data.Picture).filter((d: string) => d.startsWith('https'));
+
+      this.position = [ele.offsetTop + 20, ele.offsetLeft];
+      this.isActivitySelected = true;
+      this.isTooltipSHow = true;
+
+      this.$emit('onMarkerClick', data);
+    }
+    showMoreActivity(ele: HTMLElement, data: activity[]): void {
+      this.selectedDateActivity = data;
+      this.position = [ele.offsetTop + 20, ele.offsetLeft];
+
+      this.isTooltipSHow = !this.isTooltipSHow;
+    }
+    clearSelectedActivity(): void {
+      console.log('clear');
+      this.info = {
+        ActivityID: '',
+        ActivityName: '',
+        City: '',
+        Description: '',
+        EndTime: '',
+        Location: '',
+        Organizer: '',
+        Picture: {},
+        Position: {
+          GeoHash: '',
+          PositionLat: 0,
+          PositionLon: 0,
+        },
+        SrcUpdateTime: '',
+        StartTime: '',
+        UpdateTime: '',
+      };
+
+      this.isActivitySelected = !this.isActivitySelected;
     }
 
     // computed
