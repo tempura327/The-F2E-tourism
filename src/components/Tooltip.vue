26c6b5ea3a1cf5e56f65a703603518cde2801477
tempura327 <cks6658324@yahoo.com.tw>
2022-08-30 22:06:43 +0800

    [style] finished style of Tooltip

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="tooltip flex flex-col rounded bg-white shadow-lg shadow-gray-80/30" :style="style" v-show="isShow">
-    <slot name="control">
-      <button @click="onCloseClick" class="ml-auto">
-        <font-awesome-icon class="text-white ml-auto mb-2 hover:scale-150 cursor-pointer" icon="xmark" />
-      </button>
-    </slot>
+  <div class="tooltip" v-show="isShow">
+    <div class="tooltip_body">
+      <slot name="control">
+        <button @click="onCloseClick" class="ml-auto">
+          <font-awesome-icon class="tooltip_body_close" icon="xmark" />
+        </button>
+      </slot>
 
-    <slot></slot>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+  import { Component, Vue, Prop } from 'vue-property-decorator';
 
   @Component({ components: {} })
   export default class Tooltip extends Vue {
     // props
     @Prop({ default: false }) isShow!: boolean;
-    @Prop() position!: number[];
 
     // data
-    style = '';
 
     // methods
     onCloseClick(): void {
@@ -28,24 +28,5 @@
     }
 
     // watch
-    @Watch('position')
-    positionWatch(val: number[]): void {
-      this.style = `top:${val[0]}px; left:${val[1]}px`;
-    }
-
-    // computed
-    // get style(): string {
-    //   console.log(this.position);
-    //   return `top:${this.position[0]}; left:${this.position[1]}`;
-    // }
   }
 </script>
-
-<style scoped>
-  .tooltip {
-    width: 460px;
-    padding: 20px;
-    position: absolute;
-    z-index: 100;
-  }
-</style>
